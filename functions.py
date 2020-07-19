9ad43fc02abe1e0d8ed0783919ecc868bb1094ef
Anoop Jacob <anupjakob@gmail.com>
2020-07-19 16:47:06 +0530

    added all functions required

--- functions.py
+++ functions.py
@@ -127,11 +127,128 @@ def train_model(model , epochs, trainloader, validloader, criterion, optimizer, 
                 model.train()
     return model, optimizer
 
+# loading and processing image to use in the final stage of prediction
+# this function gets a image path and converts it into tensor and then to numpy array of optimum requirements
+def process_image(image):
+    ''' Scales, crops, and normalizes a PIL image for a PyTorch model,
+        returns an Numpy array
+    '''
+    
+    # DONE: Process a PIL image for use in a PyTorch model
+
+    # Converting image to PIL image
+    pil_im = Image.open(f'{image}' + '.jpg')
+
+    # transforming
+    transform = transforms.Compose([transforms.Resize(256),
+                                    transforms.CenterCrop(224),
+                                    transforms.ToTensor(),
+                                    transforms.Normalize([0.485, 0.456, 0.406], 
+                                                         [0.229, 0.224, 0.225])]) 
+    
+    pil_tfd = transform(pil_im)
+    
+    # Converting to Numpy array 
+    array_im_tfd = np.array(pil_tfd)
+    
+    #return array_im_tfd
+    
+    # Converting to torch tensor from Numpy array
+    img_tensor = torch.from_numpy(array_im_tfd).type(torch.FloatTensor)
+    # Adding dimension 
+    img_add_dim = img_tensor.unsqueeze_(0)
+    
+    return img_add_dim
+
+def test_model(model, testloader, gpu_mode):
+    correct = 0
+    total = 0
+    
+    if gpu_mode == True:
+        model.to('cuda')
+    else:
+        pass
+
+    with torch.no_grad():
+        # loading iterables from the test loader
+        for ii, (images, labels) in enumerate(testloader):
+            
+            if gpu_mode == True:
+                images, labels = images.to('cuda'), labels.to('cuda')
+            else:
+                pass
+            
+            outputs = model(images)
+            _, predicted = torch.max(outputs.data, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+            
+    print(f"Test accuracy of model for {total} images: {round(100 * correct / total,3)}%")
+
+
+    
+def save_model(model, train_data, optimizer, save_dir, epochs):
+    
+    checkpoint = {'state_dict': model.state_dict(),
+                  'classifier': model.classifier,
+                  'class_to_idx': train_data.class_to_idx,
+                  'opt_state': optimizer.state_dict,
+                  'num_epochs': epochs}
+
+    return torch.save(checkpoint, save_dir)
 
 
 
+def load_checkpoint(model, save_dir, gpu_mode):
+    
+    
+    if gpu_mode == True:
+        checkpoint = torch.load(save_dir)
+    else:
+        checkpoint = torch.load(save_dir, map_location=lambda storage, loc: storage)
+    
+    model.classifier = checkpoint['classifier'] 
+    model.load_state_dict(checkpoint['state_dict'])
+    model.class_to_idx = checkpoint['class_to_idx']
+    
+    return model
+            
+    
+   
+def predict(processed_image, loaded_model, topk, gpu_mode):
+    
+  
+    # evaluation mode
+    loaded_model.eval()
+    
+    if gpu_mode == True:
+        loaded_model.to('cuda')
+    else:
+        loaded_model.cpu()
+    
+    # turning gradient off
+    with torch.no_grad():
+        # Running image through network
+        output = loaded_model.forward(processed_image)
 
+    # Calculating probabilities
+    probs = torch.exp(output)
+    probs_top = probs.topk(topk)[0]
+    index_top = probs.topk(topk)[1]
+    
+    # Converting it into np array or lists
+    probs_top_list = np.array(probs_top)[0]
+    index_top_list = np.array(index_top[0])
+    
+    # Loading index and class mapping
+    class_to_idx = loaded_model.class_to_idx
+    
+    # Inverting index-class dictionary
+    indx_to_class = {x: y for y, x in class_to_idx.items()}
 
-
-
-
+ 
+    classes_top_list = []
+    for index in index_top_list:
+        classes_top_list += [indx_to_class[index]]
+        
+    return probs_top_list, classes_top_list
